6de2cb4b37a2ad276f42d361d786ae381bae7236
John Iverson Santos <153043111+JohnIversonSantos@users.noreply.github.com>
2024-01-05 06:19:40 +0800

    Modified likha login JS Added sql for backup

--- app/view/login-likha/login-likha.js
+++ app/view/login-likha/login-likha.js
@@ -1,10 +1,55 @@
 function loginWithLikha() {
     const email = document.getElementById("likha_email").value;
     const password = document.getElementById("likha_password").value;
-    const apiKey = "J7hP2fR1dVgQ9sX4tY0aL6mB3nZ8cO5";
+    const api_url = "https://likha.website/api.php";
     const applicationName = "Likha";
 
-    fetch(apiKey, {
+    fetch(api_url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: `email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`,
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const contentType = response.headers.get("content-type");
+            if (contentType && contentType.includes("application/json")) {
+                return response.json();
+            } else {
+                return response.text();
+            }
+        })
+        .then((data) => {
+            console.log("Data:", data);
+
+            if (data.status === "success") {
+                console.log("Likha Login Successful");
+
+                const userId = data.data.user_id;
+                const username = data.data.username;
+                const email = data.data.email;
+                const redirectURL = `../authorization/authorization-page.php?userName=${encodeURIComponent(username)}&email=${encodeURIComponent(email)}&app_likha=${encodeURIComponent(applicationName)}`;
+                window.location.href = redirectURL;
+            } else {
+                console.error("Likha Login failed:", data);
+            }
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+}
+
+
+
+/*
+
+const apiKey = "J7hP2fR1dVgQ9sX4tY0aL6mB3nZ8cO5";
+
+fetch(apiKey, {
             method: "POST",
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
@@ -38,7 +83,7 @@ function loginWithLikha() {
             username
           )}&email=${encodeURIComponent(
             email
-          )}&applicationName=${encodeURIComponent(applicationName)}`; //rename this
+          )}&app_likha=${encodeURIComponent(applicationName)}`; //rename this
                 window.location.href = redirectURL;
             } else {
                 console.error("Likha Login failed:", data);
@@ -48,10 +93,6 @@ function loginWithLikha() {
             console.error("Error:", error);
         });
 }
-
-
-/*
-
 
 
 */
