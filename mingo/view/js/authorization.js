fea289cc7db55c78684d0f1ec2b335e32613ec54
Ed Judah Mingo <edjudah.ej@gmail.com>
2024-01-20 15:49:18 +0800

    add modified files

---
+++ mingo/view/js/authorization.js
@@ -0,0 +1,7 @@
+function allowAccess() {
+    alert("Access granted!"); // Replace this with your desired action
+}
+
+function redirectToLogin() {
+    window.location.href = "login_as.html"; // Replace with the actual path
+}
