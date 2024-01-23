fc54424d363ad68e6498764c90ded919bd1205eb
John Iverson Santos <153043111+JohnIversonSantos@users.noreply.github.com>
2024-01-24 02:53:29 +0800

    updae profile get posts

--- app/home/php/post_get.php
+++ app/home/php/post_get.php
@@ -3,14 +3,22 @@ require "../../../db_conn.php";
 
 session_start();
 $user_id = $_SESSION['user_id'];
-$sql = "SELECT post_id, post_content 
-        FROM user_post 
-        WHERE user_id='$user_id'
-        ORDER BY created_at";
-$sql_result = $conn->query($sql);
+
+$sql = "SELECT up.user_id, up.post_id, up.post_content, it.image_path
+        FROM user_post up
+        LEFT JOIN image_table it ON up.post_id = it.post_id
+        WHERE up.user_id = ?
+        ORDER BY up.created_at";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $user_id);
+$stmt->execute();
+$sql_result = $stmt->get_result();
+
 $post_list;
 for ($i = 0; $sql_row = $sql_result->fetch_assoc(); $i++) {
         $post_list[$i] = array(
+                'username' => get_username($sql_row['user_id']), 
                 'postID' => $sql_row['post_id'],
                 'postContent' => $sql_row['post_content']
         );
--- app/home/php/post_get_all.php
+++ app/home/php/post_get_all.php
@@ -30,38 +30,4 @@ function get_username($user_id) {
         return $sql_row['username'];
 }
 
-// require "../../../db_conn.php";
-
-// session_start();
-// $user_id = $_SESSION['user_id'];
-
-// $sql = "SELECT up.user_id, up.post_content, it.image_path
-//         FROM user_post up
-//         LEFT JOIN image_table it ON up.post_id = it.post_id
-//         ORDER BY up.created_at";
-
-// $sql_result = $conn->query($sql);
-
-// $post_list = array();
-
-// while ($sql_row = $sql_result->fetch_assoc()) {
-//     $post_list[] = array(
-//         'username' => get_username($sql_row['user_id']),
-//         'userPost' => $sql_row['post_content'],
-//         'imagePath' => $sql_row['image_path']
-//     );
-// }
-
-// $response['postList'] = $post_list;
-// $response['success'] = true;
-// echo json_encode($response);
-
-// function get_username($user_id) {
-//     global $conn;
-//     $sql = "SELECT username FROM user_register WHERE user_id=$user_id";
-//     $sql_result = $conn->query($sql);
-//     $sql_row = $sql_result->fetch_assoc();
-//     return $sql_row['username'];
-// }
-
 ?>
