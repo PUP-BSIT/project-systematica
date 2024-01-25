f43a4bff4c565f1e3a15b1eb2208bd6688891d71
John Iverson Santos <153043111+JohnIversonSantos@users.noreply.github.com>
2024-01-25 20:52:04 +0800

    cookies

--- app/home/home-controllers/logout.php
+++ app/home/home-controllers/logout.php
@@ -1,11 +1,3 @@
 <?php 
-session_start();
-
-$cookie_name_email = "email";
-setcookie($cookie_name_email, "", time() - 3600, "/");
-
-session_unset();
-session_destroy();
-
 header("Location: ../../login.html");
 ?>
